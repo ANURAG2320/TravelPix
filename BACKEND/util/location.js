61db606b3b39fccfa17d8a40d9c330d5938f2e9b
ANURAG2320 <anuragbest8@gmail.com>
2024-09-29 10:13:36 +0530

    first commit

---
+++ BACKEND/util/location.js
@@ -0,0 +1,11 @@
+const axios = require('axios')
+
+
+async function getCoordsForAddress(address) {
+    return{
+        lat: 40.7484474,
+        lng: -73.9871516
+    }
+}
+
+module.exports = getCoordsForAddress;
